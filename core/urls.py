5b9b861608c93d389c8377d119fb379fdd64dbd7
Andi Milhomme <andi@anditherobot.com>
2020-05-26 02:01:40 -0400

    more stubbing

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,11 @@ from core.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('page<int:page>/', index, name='feed'),
+    path('about/', about, name='about'),
+    path('tech/', tech, name='tech'),
+    path('hobbies/', hobbies, name='hobbies'),
+    path('feed/', feed, name='feed'),
+    path('contact/', contact, name='contact'),
     #path('accounts/profile/', login_required(profile_self), name='profile'),
     #path('accounts/<str:username>/', profile_others, name='profile_others'),
 
--- core/views.py
+++ core/views.py
@@ -8,3 +8,40 @@ def index(request):
 
     context = {'title': title}
     return render(request, template_name, context)
+
+
+def about(request):
+    template_name = 'core/about.html'
+    title = "about page"
+
+    context = {'title': title}
+    return render(request, template_name, context)
+
+
+def tech(request):
+    template_name = 'core/tech.html'
+    title = "tech page"
+
+    context = {'title': title}
+    return render(request, template_name, context)
+
+def hobbies(request):
+    template_name = 'core/hobbies.html'
+    title = "hobbies page"
+
+    context = {'title': title}
+    return render(request, template_name, context)
+
+def feed(request):
+    template_name = 'core/feed.html'
+    title = "feed page"
+
+    context = {'title': title}
+    return render(request, template_name, context)
+
+def contact(request):
+    template_name = 'core/contact.html'
+    title = "contact page"
+
+    context = {'title': title}
+    return render(request, template_name, context)
