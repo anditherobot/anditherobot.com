7928de26dce595496032d0953f7fc56021715fd2
andi <andi@anditherobot.com>
2020-06-14 18:51:37 -0400

    stop settings

--- anditherobot/settings.py
+++ anditherobot/settings.py
@@ -116,7 +116,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
