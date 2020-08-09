57b774e0fc44efa0381d4eda3d0cf1a5afa6bc04
andi <andi@anditherobot.com>
2020-08-08 22:11:11 -0400

    moved invocation to here

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -1,4 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function(){
     
-    $('.hero-caption').fadeIn(2000).removeClass('hidden');
-});
+    //This is for the photo gallery
+    $('ul.first').bsPhotoGallery({
+      "classes" : "col-xl-3 col-lg-2 col-md-4 col-sm-4",
+      "hasModal" : true,
+      "shortText" : false  
+    });
+
+
+    console.log('initiallized');
+  });
