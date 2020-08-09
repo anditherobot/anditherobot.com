e5640deea7c1dd65e836c948e5987828619a6b00
andi <andi@anditherobot.com>
2020-08-08 22:50:13 -0400

    moved css around and js

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
     $('ul.first').bsPhotoGallery({
       "classes" : "col-xl-3 col-lg-2 col-md-4 col-sm-4",
       "hasModal" : true,
-      "shortText" : false  
+      "shortText" : false,
+      "showControl": true
     });
 
 
